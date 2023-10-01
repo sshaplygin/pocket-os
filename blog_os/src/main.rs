bb3f8c5098fff83b32bb56fd21e32c7ecc85a04a
Saimon Shaplygin <semyuon@gmail.com>
2023-10-01 23:58:16 +0400

    update main

---
+++ blog_os/src/main.rs
@@ -0,0 +1,35 @@
+
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello Wrold!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb800 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 +1) = 0xb;
+        }
+    }
+
+    unsafe { exit_qemu(); }
+
+    loop {}
+}
+
+pub unsafe fn exit_qemu() {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::<u32>::new(0xf4);
+    port.write(54);
+}
