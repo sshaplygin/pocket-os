2d2cb07f024129fa10ec7f080e771a8ca91b2740
Saimon Shaplygin <semyuon@gmail.com>
2025-03-09 11:34:46 +0100

    add: test print for console

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -5,11 +5,23 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+
+mod serial;
 mod vga_buffer;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QuemuExitCode::Failed);
     loop {}
 }
 
@@ -40,17 +52,30 @@ pub fn exit_qemu(exit_code: QuemuExitCode) {
 }
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QuemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial aasertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) -> () {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
 }
