f4ed2c018bc32a56bc7f76ee66692fbf816ecd98
Saimon Shaplygin <semyuon@gmail.com>
2025-03-17 20:33:46 +0100

    add: finish test framework

--- blog_os/tests/basic_boot.rs
+++ blog_os/tests/basic_boot.rs
@@ -9,22 +9,16 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
